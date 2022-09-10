7bdf3a2fc830ad3b65fec037d56bb4b3f09cb0ef
FerrariRoma <win9592@gmail.com>
2022-09-10 13:16:05 +0900

    폰켓몬 복습

--- js_bja/sparta/포켓몬.js
+++ js_bja/sparta/포켓몬.js
@@ -1,14 +1,32 @@
-// 포켓몬
-function solution(nums) {
-  // 최대 가질 수 있는 포켓몬
-  const len = nums.length / 2;
-  // 중복 제거
-  const modifiedArr = [...new Set(nums)];
-  // 중복을 제거한 길이
-  const modifiedLen = modifiedArr.length;
-  // 중복이 제거된 포켓몬의 수가 최대 가질 수 있는 포켓몬의 수 보다 작거나 같으면 중복이 제거된 포켓몬의 수를 다 가질 수 있고,
-  // 중복이 제거된 포켓몬의 수가 더 많으면 최대치를 가질 수 있음
-  return len >= modifiedLen ? modifiedLen : len;
+// // 포켓몬
+// function solution(nums) {
+//   // 최대 가질 수 있는 포켓몬
+//   const len = nums.length / 2;
+//   // 중복 제거
+//   const modifiedArr = [...new Set(nums)];
+//   // 중복을 제거한 길이
+//   const modifiedLen = modifiedArr.length;
+//   // 중복이 제거된 포켓몬의 수가 최대 가질 수 있는 포켓몬의 수 보다 작거나 같으면 중복이 제거된 포켓몬의 수를 다 가질 수 있고,
+//   // 중복이 제거된 포켓몬의 수가 더 많으면 최대치를 가질 수 있음
+//   return len >= modifiedLen ? modifiedLen : len;
+// }
+
+// solution([3, 3, 3, 2, 2, 4]);
+
+// 폰켓몬
+/**
+ *
+ * @param {*} poketmons Array : 포켓몬 별로 번호가 매겨져 있음
+ * @returns
+ */
+function solution(poketmons) {
+  const length = Math.floor(poketmons.length / 2);
+  const modifiedSetLength = [...new Set(poketmons)].length;
+  if (length >= modifiedSetLength) return modifiedSetLength;
+  return length;
 }
 
-solution([3, 3, 3, 2, 2, 4]);
+const poketmons = [3, 1, 2, 3];
+// const poketmons = [3, 3, 3, 2, 2, 4];
+// const poketmons = [3,3,3,2,2,2];
+solution(poketmons);
--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-const arr = [1, 2];
-// const test = {
-//   1: "one",
-//   2: "two",
-// };
-// for (let a of test) {
-//   console.log(a);
-// }
-console.log(arr[Symbol.iterator]);
-debugger;
-
-const obj = arr[Symbol.iterator]();
-debugger;
-console.log(obj.next());
-console.log(obj.next());
-console.log(obj.next()); // 마지막 것도 값이 나온다!
-
-/* for (let value of arr) {
-  console.log(value);
+function outter() {
+  let a = 1;
+  function inner() {
+    console.log(a);
+  }
+  return inner();
 }
-for (let value in arr) {
-  console.log(arr[value]);
-} */
+outter();
