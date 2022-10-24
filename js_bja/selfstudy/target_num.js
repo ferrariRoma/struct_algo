f9770682f3f4e44b25fce9ea104f06fdb39e61e4
FerrariRoma <win9592@gmail.com>
2022-10-25 07:57:04 +0900

    타겟 넘버 문제에 대한 설명 작성

--- js_bja/selfstudy/target_num.js
+++ js_bja/selfstudy/target_num.js
@@ -41,11 +41,20 @@
 } */
 
 // 벨로그 정리 전 복습
+/* 
+주어진 숫자 배열을 가지고 그 요소들을 모두 더하고 빼서 최종적으로 특정 값이 되는지에 관한 문제 입니다.
+주어진 배열 안에서 특정 값이 되는 경우가 몇 가지 있는지, 즉 경우의 수와 관련된 문제 입니다.
+
+solution 안에 DFS 함수를 만들었습니다. 이 함수는 재귀함수로써 문제에서 주어진 배열의 요소를 하나씩 더하고 빼고, 그 값을 다시 인자로 넘겨서 다음 요소에 동일한 작업을 진행해줍니다.
+배열의 끝까지 더했고, 그리고 그 상태에서 타겟값과 같다면 해당 경우의 수를 카운트 해주고 재귀를 종료합니다.
+
+시간 복잡도는 트리의 모든 leaf node까지 탐색해야 하기 때문에 O(2^배열의 길이)입니다.
+*/
 function solution(numbers, target) {
   let answer = 0;
   const length = numbers.length;
 
-  const DFS = (count, sum = numbers[0]) => {
+  const DFS = (count, sum) => {
     if (count === length) {
       if (sum === target) answer++;
       return;
@@ -54,7 +63,7 @@ function solution(numbers, target) {
     DFS(count + 1, sum - numbers[count]);
   };
 
-  DFS(1);
+  DFS(0, 0);
   return answer;
 }
 
