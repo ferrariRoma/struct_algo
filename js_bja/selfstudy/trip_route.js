ea7b2a9b3bcc110625c9063cb07088c9b5154376
FerrariRoma <win9592@gmail.com>
2022-10-31 05:42:31 +0900

    PC로 이동해서 작업

--- js_bja/selfstudy/trip_route.js
+++ js_bja/selfstudy/trip_route.js
@@ -69,25 +69,30 @@
   DFS("ICN", tickets, []);
   return answer.sort()[0];
 } */
-function solution(tickets) {
-  let answer = [];
 
-  const DFS = (departure, tickets, prevStop) => {
-    const nextStop = [...prevStop, departure];
-    if (tickets.length === 0) {
-      return answer.push(nextStop);
-    } else {
-      tickets.map((ticket, idx) => {
-        if (departure === ticket[0]) {
-          const copied = [...tickets];
-          const [[from, to]] = copied.splice(idx, 1);
-          DFS(to, copied, nextStop);
-        }
-      });
-    }
-  };
-  DFS("ICN", tickets, []);
-  return answer.sort()[0];
+/* 
+문제에 대한 이해
+이 문제는 가진 티켓을 모두 사용을 한 다음, 
+만일 가능한 경로가 2개 이상일 경우 있으면 알파벳 순서에 따라 정렬을 해주면 되는 문제입니다.
+
+원래 문제는 '주어진 항공권을 모두 이용하여 여행경로를 짜려고 한다. 
+항공권 정보가 담긴 2차원 배열 tickets가 매개변수로 주어질 때, 
+방문하는 공항 경로를 배열에 담아서 return하도록 solution 함수를 작성해 한다. 
+만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 한다.'
+
+나는 처음에 BFS인 줄 알았다. 
+왜냐하면 알파벳 순서대로 방문을 해야 한다고 조건이 달려 있었기 때문이다. 
+그래서 최적의 경로, 최단 경로의 느낌이 났다.
+그런데 생각해보면 모든 경우의 수 중에 티켓을 다 쓴 경로, 
+그 중에서도 알파벳 순서로 정렬해서 경로를 찾아내야 한다. 그래서 이 문제는 DFS문제이다.
+
+*/
+function solution(tickets) {
+  const getTickets = [...tickets];
+
+  const DFS = (departure, route) => {};
+
+  DFS("ICN");
 }
 
 console.log(
