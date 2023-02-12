125153c79ac0cfba4fa344fcacd805b40c8b4727
DongGyu <85842907+ferrariRoma@users.noreply.github.com>
2023-02-12 17:33:44 +0900

    Time: 59 ms (77.78%), Space: 44.8 MB (22.22%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.ts
+++ 1328-break-a-palindrome/1328-break-a-palindrome.ts
@@ -3,43 +3,24 @@ function breakPalindrome(palindrome: string): string {
 
     let answer = [...palindrome].map((el)=>el.charCodeAt(0));
     const onlyA = answer.filter((el)=>el===97);
-    if(onlyA.length===answer.length) {
-        answer[answer.length-1] = 98;
-        return answer.map((el)=>String.fromCharCode(el)).join("");
-    } else if(onlyA.length===answer.length-1) {
+    if(onlyA.length===answer.length || onlyA.length===answer.length-1) {
         answer[answer.length-1] = 98;
         return answer.map((el)=>String.fromCharCode(el)).join("");
     }
 
     let count = 0;
     let ascii = 97;
-    if(answer.length%2) {
-        count = 0;
-        while(count<answer.length/2) {
-            ascii = 97;
-            while(ascii<=122) {
-                if(answer[count]>ascii) {
-                    answer[count] = ascii;
-                    return answer.map((el)=>String.fromCharCode(el)).join('');
-                }
-                ascii++;
-            }
 
-            count++;
-        }
-    } else {
-        count = 0;
-        while(count<Math.floor(answer.length/2)) {
-            ascii = 97;
-            while(ascii<=122) {
-                if(answer[count]>ascii) {
-                    answer[count] = ascii;
-                    return answer.map((el)=>String.fromCharCode(el)).join('');
-                }
-                ascii++;
+    count = 0;
+    while(count<Math.floor(answer.length/2)) {
+        ascii = 97;
+        while(ascii<=122) {
+            if(answer[count]>ascii) {
+                answer[count] = ascii;
+                return answer.map((el)=>String.fromCharCode(el)).join('');
             }
-
-            count++;
+            ascii++;
         }
+        count++;
     }
 };
