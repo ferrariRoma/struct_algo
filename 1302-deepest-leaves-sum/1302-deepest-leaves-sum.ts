d80b80a2e1b717d23cb861e56392af753e1c984d
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2023-04-01 20:00:35 +0900

    Time: 200 ms (31.87%), Space: 65.1 MB (59.34%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function deepestLeavesSum(root: TreeNode | null): number {
+
+  const checkFn = (root:TreeNode|null, pot:number[],count:number = 0):number[] => {
+    if(!root) {
+      pot.push(count)
+      return pot;
+    };
+
+    count+=1;
+    checkFn(root.left, pot, count);
+    checkFn(root.right, pot, count);
+  }
+
+  const sumFn = (root:TreeNode|null, max:number, maxPot:number[], count:number=0) => {
+    if(!root) return;
+
+    count++;
+    if(max === count) {
+      return maxPot.push(root.val);
+    }
+
+    sumFn(root.left, max, maxPot, count);
+    sumFn(root.right, max, maxPot, count);
+  }
+
+  let pot:number[] = [];
+  checkFn(root, pot);
+
+  const MAX = Math.max(...pot);
+  let maxPot:number[] = [];
+  sumFn(root, MAX, maxPot);
+
+  return maxPot.reduce((acc:number, el:number) => acc+el);;
+};
