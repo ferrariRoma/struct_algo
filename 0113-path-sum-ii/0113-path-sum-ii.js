5ab06fb3897b15a2e8240c78b5129727f8f657c9
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2024-08-30 05:59:51 +0900

    Time: 76 ms (32.41%), Space: 52 MB (94.84%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+    const result = [];
+    let stack = [];
+    const recursiveHelper = (node) => {
+        if(node?.val !== undefined) stack.push(node.val);
+        if(!node?.left && !node?.right && stack.length) {
+           const sum = stack.reduce((acc, curr) => acc+=curr);
+           return sum === targetSum ? result.push([...stack]) : undefined;
+        }
+
+        if(node?.left) {
+            recursiveHelper(node?.left);
+            if(stack.length > 1) stack.pop();
+        }
+        if(node?.right) {
+            recursiveHelper(node?.right);
+            stack.pop();
+        }
+    }
+    recursiveHelper(root);
+    return result;
+};
