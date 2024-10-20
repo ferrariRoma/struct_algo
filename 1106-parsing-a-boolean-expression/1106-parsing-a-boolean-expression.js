1b19ac01dd69b95c32fefad57bda56051965f0dc
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2024-10-21 00:51:55 +0900

    Time: 5 ms (100.00%), Space: 54.4 MB (34.78%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.js
@@ -0,0 +1,57 @@
+/**
+ * @param {string} expression
+ * @return {boolean}
+ */
+var parseBoolExpr = function(expression) {
+    const s = [];
+    let idx = 0, curr;
+    const recur = () => {
+        const arr = [];
+        let result, p;
+        while(true) {
+            p = s.pop();
+            arr.push(p);
+            if(p === '!' || p === '&' || p === '|')
+                break;         
+        }
+        const oper = arr.pop();
+        switch(oper) {
+            case '!':
+                result = !arr.pop();
+                break;
+            case '&':
+                result = arr.reduce((acc, cur) => acc && cur);
+                break;
+            case '|':
+                result = arr.reduce((acc, cur) => acc || cur);
+                break;
+        }
+        s.push(result);
+        return result;
+    }
+    let ans;
+    while(idx !== expression.length) {
+        curr = expression[idx];
+        switch(curr) {
+            case '!':
+            case '&':
+            case '|':
+                s.push(curr);
+                break;
+            case '(':
+                break;
+            case ')':
+                // 호출
+                recur();
+                break;
+            case 't':
+                s.push(true);
+                break;
+            case 'f':
+                s.push(false);
+                break;
+        }
+        idx++;
+    }
+    return s[0];
+};
