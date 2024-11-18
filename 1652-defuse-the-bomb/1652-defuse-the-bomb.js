85017df57929479c27297b395289da5c27a06521
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2024-11-18 16:41:26 +0900

    Time: 0 ms (100.00%), Space: 50.4 MB (43.51%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.js
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -7,22 +7,20 @@ var decrypt = function(code, k) {
     const n = code.length;
     const ans = Array(n).fill(0);
     if(k == 0) return ans;
-        
     let start, end, sum = 0;
-    if(k<0) {
-       start = n+k;
-       end = n-1;
-    } else {
-       start = 1;
+    if(k > 0) {
+        start = 1;
         end = k;
+    } else {
+        start = n+k;
+        end = n-1;
     }
     
     for(let i = start; i <= end; i++) {
-        sum+=code[i%n];
+        sum += code[i%n];
     }
     for(let i = 0; i < n; i++) {
         ans[i] = sum;
-        
         sum -= code[start%n];
         start++;
         end++;
