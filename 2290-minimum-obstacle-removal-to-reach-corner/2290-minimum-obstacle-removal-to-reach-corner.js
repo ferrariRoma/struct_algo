48c3babb7ad28be1464939d18e1c3bcaa93660b0
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2024-12-02 20:55:48 +0900

    Time: 664 ms (31.49%), Space: 89.6 MB (31.14%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.js
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.js
@@ -2,71 +2,74 @@
  * @param {number[][]} grid
  * @return {number}
  */
+class Node {
+    constructor(value, priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
+class DQ {
+    constructor() {
+        this.data = {};
+        this.front = 0;
+        this.back = 0;
+    }
+    pushFront(val) {
+        const newNode = new Node(val, 0);
+        this.front--;
+        this.data[this.front] = newNode;
+    }
+    pushBack(val) {
+        const newNode = new Node(val, 1);
+        this.data[this.back] = newNode;
+        this.back++;
+    }
+    popFront() {
+        if(this.#isEmpty()) return undefined;
+        const popNode = this.data[this.front];
+        delete this.data[this.front];
+        this.front++;
+        return popNode.value;
+    }
+    popBack() {
+        if(this.#isEmpty()) return undefined;
+        this.back--;
+        const popNode = this.data[this.back];
+        delete this.data[this.back];
+        return popNode.value;
+    }
+    #isEmpty() {
+        return this.front === this.back;
+    }
+    getSize() {
+        return this.back-this.front;
+    }
+}
 var minimumObstacles = function(grid) {
-    /*
-    이것도 BFS같음. 큐를 이용해야겠지?
-    지난 번은 크기가 정해져 있었는데, 이 문제는 100000개의 m,n constraints가 있으니까
-    어쩌면 DP로 풀어야 할지도?
-    
-    일단 문제는 빈 칸으로 이동을 해서 왼쪽 상단 모서리에서
-    오른쪽 하단 모서리로 이동을 해야 합니다.
-    근데 이때 장애물이 길을 막고 있어서 갈 수 없는 경우엔,
-    장애물을 제거하면서 목적지로 갈 수 있습니다.
-    이때 몇 개의 장애물을 제거해야 최소한으로 제거하면서 목적지에 갈 수 있을까요?
-    
-    m은 세로, n은 가로
-    
-    일단은 BFS로 풀어봅시다.
-    */
-    const q = [];
-    let count = 0;
-    const m = grid.length-1;
-    const n = grid[0].length-1;
-    const vis = Array.from(Array(m+1), ()=>Array(n+1).fill(-1));
-    const dir = [[0,1], [0,-1], [1,0], [-1,0]];
-    
-    // init
-    q.push([0,0]);
+    const w = grid[0].length;
+    const h = grid.length;
+    const vis = Array.from(Array(h), ()=>Array(w).fill(Number.MAX_SAFE_INTEGER));
+    const deque = new DQ();
+    const dir = [[1,0],[0,1],[-1,0],[0,-1]];
+    deque.pushFront([0,0],0);
     vis[0][0] = 0;
-    
-    // BFS
-    while(q.length>0) {
-        const leng = q.length;
-        for(let i = 0; i < leng; i++) {
-            const [x,y] = q.shift();
-            // TODO : 탈출 조건 작성해야 함! 어쩌면 코드 마지막에 해야 할지도??
-            // if(n === x && m === y) return ;
-            const openPath = [];
-            const closePath = [];
-            for(let j = 0; j < 4; j++) {
-                const nextY = y + dir[j][0];
-                const nextX = x + dir[j][1];
-                if(nextX >= 0 && nextX <= n && nextY >= 0 && nextY <= m) {
-                    const nextPos = [nextX, nextY];
-                    if(nextPos[0]===0 && nextPos[1]===0) continue;
-                    if(grid[nextY][nextX] === 0 && vis[nextY][nextX] !== 0) {
-                        openPath.push(nextPos);
-                    } else if(vis[nextY][nextX] !== 0) {
-                        closePath.push(nextPos);
+    while(deque.getSize()) {
+        const leng = deque.getSize();
+        const [x,y] = deque.popFront();
+        for(const [dx, dy] of dir) {
+            const [nx, ny] = [x+dx, y+dy];
+            if(nx>=0 && nx<h && ny>=0 && ny<w) {
+                const newDist = vis[x][y] + grid[nx][ny];
+                if(newDist < vis[nx][ny]) {
+                    vis[nx][ny] = newDist;
+                    if(grid[nx][ny]===1) {
+                        deque.pushBack([nx,ny]);
+                    } else {
+                        deque.pushFront([nx,ny]);
                     }
                 }
             }
-            // if(openPath.length !== 0) {
-                openPath.forEach(path=>{
-                    if(vis[path[1]][path[0]] > vis[y][x] || vis[path[1]][path[0]] === -1) {
-                        vis[path[1]][path[0]] = vis[y][x];
-                        q.push(path)
-                    };
-                });
-            // } else if(closePath.legnth !== 0) {
-                closePath.forEach(path=>{
-                    if(vis[path[1]][path[0]] > vis[y][x] + 1 || vis[path[1]][path[0]] === -1) {
-                        vis[path[1]][path[0]] = vis[y][x] + 1;
-                        q.push(path)
-                    };
-                });
-            // }
         }
     }
-    return vis[m][n];
+    return vis[h-1][w-1];
 };
