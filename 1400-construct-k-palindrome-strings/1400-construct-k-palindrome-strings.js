783ac363856f00fdd34d7ef39c170b1cf60e3799
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-01-11 18:12:36 +0900

    Time: 26 ms (60%), Space: 53.1 MB (52%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.js
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.js
@@ -5,43 +5,17 @@
  */
 var canConstruct = function(s, k) {
     if(s.length < k) return false;
-    const n = s.length, dictionary = new Map(), countOdd = {};
+    const n = s.length, dictionary = new Map();
     for(let i=0; i<n; i++) {
         dictionary.set(s[i], (dictionary.get(s[i]) ?? 0) + 1);
     }
     // 여기까지 n
 
-    let oddArray = [], totalOddNums = 0, isNotChanged=0;
+    let ans = 0;
     for(const [key, value] of dictionary) {
-        if(value%2!==0) {
-            oddArray.push([key, value]);
-            totalOddNums++;
-        };
+        if(value%2!==0) ans++
     }
     // n+oddArray.length
 
-    for(let i=0; i<k; i++) {
-        countOdd[i] = 0;
-    }
-    // n+oddArray.length+k
-
-    return oddArray.length<=k;
-    
-    // while(true) {
-    //     if(totalOddNums===0) return true;
-    //     if(totalOddNums===isNotChanged) return false;
-    //     isNotChanged = totalOddNums;
-    //     let idx=0;
-    //     oddArray = oddArray.map(([key,value])=>{
-    //         if(value%2===0) return [key,value];
-    //         idx = idx%k;
-    //         if(countOdd[idx]===0) {
-    //             countOdd[idx]++;
-    //             totalOddNums--;
-    //             value--;
-    //         }
-    //         idx++;
-    //         return [key,value];
-    //     })
-    // }
+    return ans<=k;
 };
