069b2ca45d02e11a89e9a221568c3e482c61e999
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-02-10 10:18:46 +0900

    Time: 24 ms (5.6%), Space: 55.5 MB (6%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var clearDigits = function(s) {
+    const nonDigitQ = [], deleteIndices = new Map();
+    for(let i = 0; i < s.length; i++) {
+        if(Number.isNaN(Number(s[i]))) {
+            nonDigitQ.push(i);
+        } else {
+            if(nonDigitQ.length !== 0) {
+                const nonDigit = nonDigitQ.pop();
+                deleteIndices.set(i, s[i]);
+                deleteIndices.set(nonDigit, s[nonDigit]);
+            }
+        }
+    }
+    console.log(deleteIndices);
+    let ans = '';
+    for(let i = 0; i < s.length; i++) {
+        if(!deleteIndices.has(i)) ans+=s[i];
+    }
+    return ans;
+};
