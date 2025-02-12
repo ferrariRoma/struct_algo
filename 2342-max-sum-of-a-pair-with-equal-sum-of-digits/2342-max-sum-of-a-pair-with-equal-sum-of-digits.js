4ae8413df8728b64208f471d79d4c6299cbb17e5
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-02-12 13:43:31 +0900

    Time: 96 ms (41.18%), Space: 69.3 MB (23.53%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -1,41 +1,3 @@
-class MaxPQ {
-    constructor() {
-        this.queue = [];
-    }
-    #bubbleup(idx) {
-        if(idx <= 0) return;
-        const parentIdx = Math.floor((idx-1)/2);
-        if(this.queue[parentIdx] < this.queue[idx]) {
-            [this.queue[parentIdx], this.queue[idx]] = [this.queue[idx], this.queue[parentIdx]];
-            this.#bubbleup(parentIdx);
-        }
-    }
-    #sinkdown(idx) {
-        if(idx >= this.queue.length) return;
-        const leftChildIdx = idx*2+1;
-        const rightChildIdx = leftChildIdx + 1;
-        if(this.queue[leftChildIdx] > this.queue[idx] || this.queue[rightChildIdx] > this.queue[idx]) {
-            const biggerIdx = this.queue[leftChildIdx] < this.queue[rightChildIdx] ? rightChildIdx : leftChildIdx;
-            [this.queue[biggerIdx], this.queue[idx]] = [this.queue[idx], this.queue[biggerIdx]];
-            this.#sinkdown(biggerIdx);
-        }
-    }
-    enqueue(num) {
-        this.queue.push(num);
-        this.#bubbleup(this.queue.length-1);
-        return this;
-    }
-    dequeue() {
-        [this.queue[this.queue.length-1], this.queue[0]] = [this.queue[0], this.queue[this.queue.length-1]];
-        const res = this.queue.pop();
-        this.#sinkdown(0);
-        return res;
-    }
-    get getSize() {
-        return this.queue.length;
-    }
-}
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -46,17 +8,13 @@ var maximumSum = function(nums) {
     for(let i = 0; i < nums.length; i++) {
         const sum = Number([...`${nums[i]}`].reduce((acc, curr)=>(+acc)+(+curr)));
         if(t.has(sum)) {
-            t.set(sum, t.get(sum).enqueue(nums[i]));
+            ans = Math.max(ans, nums[i] + t.get(sum));
+            t.set(sum, Math.max(t.get(sum),nums[i]));
         }
         else {
-            t.set(sum, new MaxPQ().enqueue(nums[i]));
+            t.set(sum, nums[i]);
         }
     }
     
-    for(const [_, pq] of t) {
-        if(pq.getSize < 2) continue;
-        const sum = pq.dequeue() + pq.dequeue();
-        ans = ans < sum ? sum : ans;
-    }
     return ans;
 };
