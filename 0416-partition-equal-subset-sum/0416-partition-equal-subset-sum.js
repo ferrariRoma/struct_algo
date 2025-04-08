809bc541214724ba5148cc89391f15898ec074f5
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-04-08 12:59:59 +0900

    Time: 33 ms (95.68%), Space: 59.1 MB (61.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -6,16 +6,13 @@ var canPartition = function(nums) {
     const sum = nums.reduce((acc, curr)=> acc+curr);
     if(sum%2 === 1) return false;
     const target = sum/2;
-
     const dp = Array(target+1).fill(false);
     dp[0] = true;
-
     for(const num of nums) {
         for(let i = target; i >= num; i--) {
             dp[i] = dp[i] || dp[i - num];
-            if(dp[target]) return true;
+            if(dp[target]) break;
         }
     }
-
     return dp[target];
 };
