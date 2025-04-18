d1c4261c6553b27cffe0fc9267eb8d4a249f4d2a
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-04-18 12:27:22 +0900

    Time: 51 ms (37.21%), Space: 77.6 MB (96.88%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.js
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.js
@@ -7,8 +7,8 @@ var countGood = function(nums, k) {
     const n = nums.length;
     let same = 0, right = -1, ans = 0;
     const count = new Map();
-    for(let left = 0; left < nums.length; left++) {
-        while(same < k && right < n - 1) {
+    for(let left = 0; left <= n; left++) {
+        while(same < k && right < n-1) {
             right++;
             same += count.get(nums[right]) ?? 0;
             count.set(nums[right], (count.get(nums[right]) ?? 0) + 1);
@@ -16,8 +16,8 @@ var countGood = function(nums, k) {
         if(same >= k) {
             ans += n - right;
         }
-        count.set(nums[left], count.get(nums[left])-1);
+        count.set(nums[left], count.get(nums[left]) - 1);
         same -= count.get(nums[left]);
-    }
+    }  
     return ans;
 };
