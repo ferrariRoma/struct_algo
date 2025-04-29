a3da718991e178dc0c5cb667bbec7d0823ca0c8b
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-04-30 01:42:59 +0900

    Time: 31 ms (41.79%), Space: 55.8 MB (25.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,27 +3,10 @@
  * @param {number} target
  * @return {number[]}
  */
-// I tried to use hash table things.
-// var twoSum = function(nums, target) {
-//     if(nums.length === 0) return [];
-//     const ht = Object.fromEntries(nums.map((el, idx) => [el, idx]));
-//     let result;
-//     for(let i = 0; i < nums.length; i++) {
-//         if(ht[target - nums[i]] && ht[target - nums[i]] !== i) return result = [i, ht[target - nums[i]]]
-//     }
-//     result;
-// };
-
-var twoSum = function(nums, target) {
-    let idx1, idx2;
-    for(let i = 0; i<nums.length; i++){
-        for(let j = 0; j<nums.length; j++){
-            if(i === j) continue;
-            if(nums[i] + nums[j] === target) {
-                idx1 = i, idx2 = j;
-                break;
-            }
-        }    
+var twoSum = function(nums, target) {    
+    for(let i = 0; i < nums.length - 1; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) return [i, j]
+        }
     }
-    return [idx1, idx2];
 };
