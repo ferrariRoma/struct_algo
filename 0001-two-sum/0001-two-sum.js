fa0ab1d7d8e1ba8bdd65a396859c9eb739975de3
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-04-30 14:05:11 +0900

    Time: 2 ms (70.71%), Space: 55.4 MB (38.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,12 +1,27 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {    
+//     for(let i = 0; i < nums.length - 1; i++) {
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] === target) return [i, j]
+//         }
+//     }
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {    
-    for(let i = 0; i < nums.length - 1; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) return [i, j]
+    const table = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        if(table.has(target - nums[i])) {
+            return [i, table.get(target - nums[i])]
         }
+        table.set(nums[i], i);
     }
 };
