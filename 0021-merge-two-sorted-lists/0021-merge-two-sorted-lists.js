8d696dcdd5b71b8608ea494980297bb3520be217
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-05-02 16:11:47 +0900

    Time: 0 ms (100%), Space: 58 MB (29.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,40 +1,27 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
- */
 var mergeTwoLists = function(list1, list2) {
-    let ans = null;
-
-    const recurHelper = (list1, list2, newNode) => {
-        console.log(list1, list2, newNode);
-        if(list1 === null && list2 === null) return newNode;
-        if(list1 !== null && list2 !== null) {
-            if(list1.val < list2.val) {
-                newNode = new ListNode(list1.val, undefined);
-                list1 = list1.next;
-            } else {
-                newNode = new ListNode(list2.val, undefined);
-                list2 = list2.next;
-            }
-            recurHelper(list1.next, list2.next, newNode.next);
-        } else if(list1 !== null) {
-            newNode = new ListNode(list1.val, undefined);
-            recurHelper(list1.next, list2, newNode.next);
+    let ans = new ListNode();
+    
+    function helper(l1, l2, current) {
+        if (!l1 && !l2) return;
+        
+        if (!l1) {
+            current.next = l2;
+            return;
+        }
+        if (!l2) {
+            current.next = l1;
+            return;
+        }
+        
+        if (l1.val < l2.val) {
+            current.next = l1;
+            helper(l1.next, l2, current.next);
         } else {
-            newNode = new ListNode(list2.val, undefined);
-            recurHelper(list1, list2.next, newNode.next);
+            current.next = l2;
+            helper(l1, l2.next, current.next);
         }
     }
-
-    recurHelper(list1, list2, ans);
-
-    return ans;
-};
+    
+    helper(list1, list2, ans);
+    return ans.next;
+}
