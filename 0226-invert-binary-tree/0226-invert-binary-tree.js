ef236baf03a62b45af1abbb1858d85879535521e
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-05-13 02:32:49 +0900

    Time: 0 ms (100%), Space: 54.2 MB (20.95%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,13 +11,10 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    // 내부 helper 함수 정의
     function helper(node) {
         if (node === null) return;
-        // 자식 노드들을 재귀적으로 뒤집기
         helper(node.left);
         helper(node.right);
-        // 현재 노드의 left, right를 swap
         [node.left, node.right] = [node.right, node.left];
     }
     helper(root);
