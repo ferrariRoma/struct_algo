47e924fc601cee4a8763ef76a5efefff15525b90
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-05-13 22:41:06 +0900

    Time: 8 ms (26.01%), Space: 54.4 MB (99.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase();
-    const reg = new RegExp(/^[a-z0-9]+$/);
-    const ans = [];
-    for(let i = 0; i < s.length; i++) {
-        if(reg.test(s[i])) ans.push(s[i]);
-    }
+    const ans = s.replaceAll(/[^a-z0-9]/gi, '').toLowerCase();
     for(let i = 0; i <= Math.floor(ans.length); i++) {
         if(ans[i] !== ans[ans.length-1-i]) return false;
     }
