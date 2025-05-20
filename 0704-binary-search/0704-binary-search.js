54bce72e851a19f7338d3b25777c8a52a117a815
FeRo <85842907+ferrariRoma@users.noreply.github.com>
2025-05-20 23:31:25 +0900

    Time: 0 ms (100%), Space: 58.3 MB (30.66%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -5,13 +5,14 @@
  */
 var search = function(nums, target) {
     let left = 0, right = nums.length-1;
-    while(left < right) {
+    while(left <= right) {
         const mid = Math.floor((left + right)/2);
+        if(nums[mid] === target) return mid;
         if(nums[mid] < target) {
             left = mid + 1;
         } else {
-            right = mid;
+            right = mid - 1;
         }
     }
-    return nums[left] === target ? left : -1;
+    return -1;
 };
