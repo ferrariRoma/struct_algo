67dcbe5c236beb05ed2f382d628f6061abcd7ae1
DongGyu An <85842907+ferrariRoma@users.noreply.github.com>
2025-07-05 03:55:02 +0900

    Time: 4 ms (80.12%), Space: 18 MB (19.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,14 +2,10 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        arr = []
-        while x > 0:
-            arr.append(x % 10)
-            x//=10
-        length = len(arr)
-        idx = 0
-        while idx < length//2:
-            if arr[idx] != arr[length-1-idx]:
-                return False
-            idx += 1
-        return True
+        num = x
+        ans = 0
+        while num:
+            digit = num % 10
+            ans = ans*10 + digit
+            num //= 10
+        return ans == x
