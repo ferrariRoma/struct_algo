c2738b9186d95a97151e514be33308b583b8d8f7
DongGyu An <85842907+ferrariRoma@users.noreply.github.com>
2025-08-04 00:34:05 +0900

    Time: 78 ms (33.3%), Space: 77.2 MB (14.3%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -1,24 +1,54 @@
+const MaxHeap = class {
+    queue = [];
+    enqueue(coord, dist) {
+        this.queue.push([coord, dist]);
+        this.bubbleup(this.queue.length-1);
+    }
+    bubbleup(idx) {
+        if(idx <= 0) return;
+        const parentIdx = Math.floor((idx-1)/2);
+        if(this.queue[parentIdx][1] < this.queue[idx][1]) {
+            [this.queue[parentIdx], this.queue[idx]] = [this.queue[idx], this.queue[parentIdx]];
+            this.bubbleup(parentIdx);
+        }
+    }
+    dequeue() {
+        if(this.queue.length === 0) return;
+        if(this.queue.length === 1) return this.queue.pop();
+        [this.queue[this.queue.length-1], this.queue[0]] = [this.queue[0], this.queue[this.queue.length-1]];
+        const ans = this.queue.pop()
+        this.sinkdown(0);
+        return ans;
+    }
+    sinkdown(idx) {
+        const leftChildIdx = idx*2 + 1;
+        const rightChildIdx = leftChildIdx + 1;
+        if(this.queue.length-1 < rightChildIdx) return;
+        if(this.queue[leftChildIdx][1] > this.queue[idx][1] || this.queue[rightChildIdx][1] > this.queue[idx][1]) {
+            const biggerIdx = this.queue[leftChildIdx][1] < this.queue[rightChildIdx][1] ? rightChildIdx : leftChildIdx;
+            [this.queue[idx], this.queue[biggerIdx]] = [this.queue[biggerIdx], this.queue[idx]];
+            this.sinkdown(biggerIdx);
+        }
+    }
+    getResult() {
+        return this.queue.map(coord => coord[0])
+    }
+}
+
 /**
  * @param {number[][]} points
  * @param {number} k
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    const coordinates = [];
-    const dists = [];
+    const h = new MaxHeap();
     for(const [x, y] of points) {
-        const res = x**2 + y**2;
-        if(coordinates.length === k) {
-            const max = Math.max(...dists);
-            if(max > res) {
-                const idx = dists.findIndex(dist => dist == max);
-                coordinates[idx] = [x, y];
-                dists[idx] = res;
-            }
-        } else {
-            coordinates.push([x, y]);
-            dists.push(res);
-        }
+        const dist = x**2 + y**2;   
+        h.enqueue([x, y], dist);
     }
-    return coordinates;
+
+    while(h.queue.length !== k) {
+        h.dequeue();
+    }
+    return h.getResult();
 };
